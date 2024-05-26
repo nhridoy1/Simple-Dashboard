d0b3757c5e97195ca8ee5d10e7d916707bfa68c9
Ridoy <ridoyhossain5557@gmail.com>
2024-05-26 11:54:34 +0600

    Simple Dashboard created

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Assignment1.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+       
+    }
+}
